078ab12c06c2f4a0d7e1fb45b6d6af65f83f619c
SalsaGal <fielddashos@gmail.com>
2023-10-29 22:10:34 +1100

    Give error on missing args

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("Hello World!\n");
+  if (argc != 1) {
+    printf("Incorrect argument count");
+    return 1;
+  }
+
   return 0;
 }
