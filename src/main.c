900068d2c2627a7a0ede34ef721d9c3b31c96da9
SalsaGal <fielddashos@gmail.com>
2023-10-29 22:14:25 +1100

    Correct arg errors

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  if (argc != 1) {
-    printf("Incorrect argument count");
+  if (argc != 2) {
+    printf("Incorrect argument count\n");
     return 1;
   }
 
