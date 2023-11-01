77bc76400bd8422f2fedd934137ca3288cd21fc3
SalsaGal <fielddashos@gmail.com>
2023-11-01 20:53:39 +1100

    Add second argument

--- src/main.c
+++ src/main.c
@@ -6,20 +6,23 @@
 #define BUFFER_SIZE 1024 * 1024
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
+  if (argc != 3) {
     printf("Incorrect argument count\n");
     return 1;
   }
 
-  FILE *file = fopen(argv[1], "rb");
-  char *buffer = malloc(BUFFER_SIZE);
+  FILE *snd_file = fopen(argv[1], "rb");
+  char *snd_buffer = malloc(BUFFER_SIZE);
+  size_t snd_size = fread(snd_buffer, sizeof(char), BUFFER_SIZE, snd_file);
 
-  size_t bytes = fread(buffer, sizeof(char), BUFFER_SIZE, file);
+  FILE *smp_file = fopen(argv[2], "rb");
+  char *smp_buffer = malloc(BUFFER_SIZE);
+  size_t smp_size = fread(smp_buffer, sizeof(char), BUFFER_SIZE, smp_file);
 
-  printf("%s\n", buffer);
-  printf("Bytes: 0x%lx\n", bytes);
+  printf("SND size: 0x%zx\n", snd_size);
+  printf("SMP size: 0x%zx\n", smp_size);
 
-  if (bytes == BUFFER_SIZE) {
+  if (snd_size == BUFFER_SIZE || smp_size == BUFFER_SIZE) {
     printf("WARNING: Buffer full, might not have been fully read.\n");
   }
 
