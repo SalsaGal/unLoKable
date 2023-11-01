bc1f88cd88a3f37fdcf120608fdd0cb49e1fd2df
SalsaGal <fielddashos@gmail.com>
2023-11-01 19:50:32 +1100

    Read file

--- src/main.c
+++ src/main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024 * 1024
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     printf("Incorrect argument count\n");
     return 1;
+  }
+
+  FILE *file = fopen(argv[1], "rb");
+  char *buffer = malloc(BUFFER_SIZE);
+
+  size_t bytes = fread(buffer, sizeof(char), BUFFER_SIZE, file);
+
+  printf("%s\n", buffer);
+  printf("Bytes: 0x%zu\n", bytes);
+
+  if (bytes == BUFFER_SIZE) {
+    printf("WARNING: Buffer full, might not have been fully read.\n");
   }
 
   return 0;
