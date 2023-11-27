1843103751a52c4a3939d39891a6129cd3df351b
SalsaGal <fielddashos@gmail.com>
2023-11-27 21:00:58 +1100

    Writing CDS files

--- sequence/src/main.c
+++ sequence/src/main.c
@@ -1,3 +1,4 @@
+#include "../../lib/misc.h"
 #include "../../lib/structures.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,11 +6,12 @@
 #define BUFFER_SIZE 1024 * 1024
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
+  if (argc != 3) {
     printf("Missing argument for file!\n");
     return 1;
   }
 
+  char *output_dir = argv[2];
   FILE *file = fopen(argv[1], "rb");
   char *file_buffer = malloc(BUFFER_SIZE);
   char *file_start = file_buffer;
@@ -23,7 +25,7 @@ int main(int argc, char *argv[]) {
 
   int *track_offsets = calloc(header.numTracks, sizeof(int));
   for (int i = 0; i < header.numTracks; i++) {
-  track_offsets[i] = parse_int_be(&file_buffer);
+    track_offsets[i] = parse_int_be(&file_buffer);
   }
 
   Slice *track_slices = calloc(header.numTracks, sizeof(Slice));
@@ -44,6 +46,29 @@ int main(int argc, char *argv[]) {
   printf("padding: %d\n", header.padding);
   for (int i = 0; i < header.numTracks; i++) {
     printf("Track #%d: %x, %x\n", i, track_offsets[i], track_slices[i].length);
+  }
+
+  for (int i = 0; i < header.numTracks; i++) {
+    char *output_path = malloc(128); // TODO Make this better
+    sprintf(output_path, "%s/%s_%04d.cds", output_dir, remove_path(argv[1]), i);
+    FILE *output = fopen(output_path, "wb");
+    Slice *track = &track_slices[i];
+    char header_bytes[] = {
+      0x70, 0x53, 0x44, 0x43,
+      0x0, 0x0, 0x0, 0x1,
+      (header.ppqn & 0xff00) >> 8,
+      header.ppqn & 0xff,
+      (header.quarterNoteTime & 0x00ff0000) >> 16,
+      (header.quarterNoteTime & 0x0000ff00) >> 8,
+      (header.quarterNoteTime & 0x000000ff),
+      0x4, 0x2,
+    };
+    for (int j = 0; j < 15; j++) {
+      fprintf(output, "%c", header_bytes[j]);
+    }
+    for (int j = 0; j < track->length; j++) {
+      fprintf(output, "%c", track->start[j]);
+    }
   }
 
   return 0;
