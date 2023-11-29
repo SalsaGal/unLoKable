d412a89b1bdbac6f5b746c3b9fc10a2a9242f58a
SalsaGal <fielddashos@gmail.com>
2023-11-29 21:22:25 +1100

    Copy cds body bytes

--- cds2seq/src/main.c
+++ cds2seq/src/main.c
@@ -3,8 +3,22 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+bool reached_loop_count(unsigned char *index) {
+  return (index[0] == 0xff) && (index[1] == 0x2e) && (index[2] == 0x01);
+}
+
+bool reached_loop_terminator(unsigned char *index) {
+  return (index[0] == 0xff) && (index[1] == 0x2f) && (index[2] == 0x00);
+}
+
 bool reached_terminator(unsigned char *index) {
-  return (index[0] == 0xff); // && (index[1] == 0x44) && (index[2] == 0x00);
+  return (index[0] == 0xff) && (index[1] == 0x44) && (index[2] == 0x00);
+}
+
+void copy_bytes(Vec *vec, int length, unsigned char data[]) {
+  for (int i = 0; i < length; i++) {
+    vec_push(vec, data[i]);
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -25,10 +39,42 @@ int main(int argc, char *argv[]) {
   printf("%x\n", header.quarterNoteTime[2]);
   printf("%x\n", header.timeSignature);
 
-  while (!reached_terminator(cds_index)) {
-    printf("%x\n", ((int)*cds_index) % 0xff);
-    cds_index++;
+  Vec output = vec_new(64);
+  unsigned char *copy_start = cds_index;
+  short loop_count = -1;
+  while (true) {
+    if (reached_loop_count(cds_index)) {
+      loop_count = (short) cds_index[3];
+      cds_index += 4;
+      copy_bytes(&output, cds_index - copy_start, copy_start);
+      printf("Copied 0x%lx to 0x%lx\n", copy_start - cds_buffer.start, cds_index - cds_buffer.start);
+      copy_start = cds_index;
+    } else if (reached_loop_terminator(cds_index)) {
+      cds_index += 3;
+      if (loop_count == -1) {
+        copy_bytes(&output, cds_index - copy_start, copy_start);
+      } else {
+        for (int i = 0; i < loop_count; i++) {
+          copy_bytes(&output, cds_index - copy_start, copy_start);
+        }
+      }
+      printf("Copied 0x%lx to 0x%lx, loop count %d\n", copy_start - cds_buffer.start, cds_index - cds_buffer.start, loop_count);
+      copy_start = cds_index;
+      loop_count = -1;
+    } else if (reached_terminator(cds_index)) {
+      cds_index += 3;
+      copy_bytes(&output, cds_index - copy_start, copy_start);
+      break;
+    } else {
+      cds_index++;
+    }
   }
+
+  FILE *out = fopen("test.bin", "wb");
+  for (int i = 0; i < output.length; i++) {
+    fprintf(out, "%c", output.start[i]);
+  }
+  fclose(out);
 
   return 0;
 }
