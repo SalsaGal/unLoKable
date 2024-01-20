dc013e211ee2075916bc67adf2dd327d48e800db
SalsaGal <fielddashos@gmail.com>
2024-01-20 21:40:01 +1100

    Add platform options

--- demus/src/main.rs
+++ demus/src/main.rs
@@ -2,14 +2,23 @@ use std::path::PathBuf;
 
 use clap::Parser;
 
+#[derive(Clone, Copy, clap::ValueEnum)]
+pub enum Platform {
+    Dreamcast,
+    PC,
+}
+
 #[derive(Parser)]
 #[command(version)]
 struct Args {
     /// msq file to read
     input: PathBuf,
     /// Whether to display debug information or not
-    #[clap(long, short)]
+    #[clap(short)]
     debug: bool,
+    /// What platform to use the format of
+    #[clap(short)]
+    platform: Platform,
     /// Output path of the cds file, defaults to the input with a different extension
     #[clap(long, short)]
     output: Option<PathBuf>,
