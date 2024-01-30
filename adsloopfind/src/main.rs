c109c8e69d938fe5dbfeb6f9b9687500797ddfc2
SalsaGal <fielddashos@gmail.com>
2024-01-30 22:15:01 +1100

    Correct output

--- adsloopfind/src/main.rs
+++ adsloopfind/src/main.rs
@@ -1,13 +1,24 @@
+use std::path::PathBuf;
+
 const MAGIC_NUMBER: [u8; 4] = [0x53, 0x53, 0x68, 0x64];
 
 fn main() {
-    let ads_path = std::env::args()
-        .nth(1)
-        .expect("`ads` path must be supplied");
-    let ads_file = std::fs::read(ads_path).unwrap();
+    let ads_path = PathBuf::from(
+        std::env::args()
+            .nth(1)
+            .expect("`ads` path must be supplied"),
+    );
+    let ads_file = std::fs::read(&ads_path).unwrap();
 
     let (lb, le) = find_loops(&ads_file);
-    println!("{lb} {le}");
+    print!(
+        "{lb} {le} {}\r\n",
+        ads_path
+            .with_extension("wav")
+            .file_name()
+            .unwrap()
+            .to_string_lossy()
+    );
 }
 
 fn find_loops(ads_file: &[u8]) -> (u32, u32) {
