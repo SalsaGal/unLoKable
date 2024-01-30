057a0714e10a8ef1754143c10cddc3fdaf922030
SalsaGal <fielddashos@gmail.com>
2024-01-30 22:10:12 +1100

    Finish adsloopfind

--- adsloopfind/src/main.rs
+++ adsloopfind/src/main.rs
@@ -2,14 +2,49 @@ const MAGIC_NUMBER: [u8; 4] = [0x53, 0x53, 0x68, 0x64];
 
 fn main() {
     let ads_path = std::env::args()
-        .next()
+        .nth(1)
         .expect("`ads` path must be supplied");
     let ads_file = std::fs::read(ads_path).unwrap();
 
-    if ads_file[..4] != MAGIC_NUMBER {
+    let (lb, le) = find_loops(&ads_file);
+    println!("{lb} {le}");
+}
+
+fn find_loops(ads_file: &[u8]) -> (u32, u32) {
+    if ads_file[0..4] != MAGIC_NUMBER {
         eprintln!(
             "Invalid magic number, expected {MAGIC_NUMBER:?}, found {:?}",
-            &ads_file[..4]
+            &ads_file[0..4]
         );
+        std::process::exit(1);
     }
+
+    let body_size = load_bytes(&ads_file[0x24..]);
+    let codec = load_bytes(&ads_file[8..]);
+    if codec != 0x10 {
+        return (0, 0);
+    }
+
+    let channel_number = load_bytes(&ads_file[0x10..]);
+    let step_size = 16;
+
+    let body = &ads_file[0x28..];
+
+    body.chunks(step_size as usize)
+        .enumerate()
+        .find_map(|(i, x)| {
+            if x[1] == 6 {
+                Some((
+                    i as u32 * 28 / channel_number,
+                    body_size / 16 * 28 / channel_number,
+                ))
+            } else {
+                None
+            }
+        })
+        .unwrap_or((0, 0))
+}
+
+fn load_bytes(bytes: &[u8]) -> u32 {
+    u32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]])
 }
