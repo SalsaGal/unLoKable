03d01307892e763fd9286f33ace6341d49e337bd
SalsaGal <fielddashos@gmail.com>
2024-02-26 22:14:39 +1100

    Load vag sizes

--- vabsmp/src/main.rs
+++ vabsmp/src/main.rs
@@ -24,6 +24,7 @@ struct VabFile {
     header: VabHeader,
     programs: Vec<Program>,
     tones: Vec<Vec<Tone>>,
+    vag_sizes: Vec<u16>,
 }
 
 impl VabFile {
@@ -55,10 +56,21 @@ impl VabFile {
             })
             .collect();
 
+        bytes.next().unwrap();
+        bytes.next().unwrap();
+
+        let vag_sizes: Vec<u16> = (0..header.vags_number)
+            .map(|_| u16::from_le_bytes([*bytes.next().unwrap(), *bytes.next().unwrap()]))
+            .collect();
+        for _ in 0..512 - vag_sizes.len() * 2 - 2 {
+            bytes.next().unwrap();
+        }
+
         Self {
             header,
             programs,
             tones,
+            vag_sizes,
         }
     }
 }
