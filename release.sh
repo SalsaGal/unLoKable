0334e0ab8d3b10a28997abb7f75cedc27a52343b
SalsaGal <fielddashos@gmail.com>
2024-05-01 19:33:57 +1000

    Add linux release building

---
+++ release.sh
@@ -0,0 +1,19 @@
+cargo build --release
+mkdir out
+
+cp target/release/adsheader out
+cp target/release/adsloopfind out
+cp target/release/cds2seq out
+cp target/release/demul out
+cp target/release/demus out
+cp target/release/desnd out
+cp target/release/msqsplit out
+cp target/release/seqrepeat out
+cp target/release/sf2panlaw out
+cp target/release/vabfine out
+cp target/release/vabsmp out
+cp target/release/vagheader out
+cp target/release/vagsanitizer out
+
+zip -r release.zip out
+rm -rf out
