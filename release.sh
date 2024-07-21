283a7a8e342ca27ec0e178bfdc8b6e855667c450
SalsaGal <fielddashos@gmail.com>
2024-07-21 20:15:05 +1000

    Add new targets to release.sh

--- release.sh
+++ release.sh
@@ -3,6 +3,7 @@ mkdir out
 
 cp target/release/adsheader out
 cp target/release/adsloopfind out
+cp target/release/adsunloop out
 cp target/release/cds2seq out
 cp target/release/demul out
 cp target/release/demus out
@@ -14,6 +15,7 @@ cp target/release/vabfine out
 cp target/release/vabsmp out
 cp target/release/vagheader out
 cp target/release/vagsanitizer out
+cp target/release/vagunloop out
 
 zip -r release.zip out
 rm -rf out
