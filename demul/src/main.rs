e2a5980a3b35b49c3ff29cddd27bfc9574d6dd9b
SalsaGal <fielddashos@gmail.com>
2024-09-16 19:23:34 +1000

    Add batching to demul

--- demul/src/main.rs
+++ demul/src/main.rs
@@ -6,104 +6,114 @@ use clap::Parser;
 struct Args {
     /// The `mul` file to read from.
     input: PathBuf,
-    /// The output directory
-    #[clap(short, long)]
-    output: Option<PathBuf>,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let mul_file = std::fs::read(&args.input).unwrap();
+    let file_paths: &mut dyn Iterator<Item = PathBuf> = if args.input.is_dir() {
+        &mut args
+            .input
+            .read_dir()
+            .unwrap()
+            .flatten()
+            .map(|dir| dir.path())
+    } else {
+        &mut std::iter::once(args.input)
+    };
 
-    let sample_rate = u32::from_le_bytes([mul_file[0], mul_file[1], mul_file[2], mul_file[3]]);
-    let channels = u32::from_le_bytes([mul_file[12], mul_file[13], mul_file[14], mul_file[15]]);
+    for file_path in file_paths {
+        let mul_file = std::fs::read(&file_path).unwrap();
 
-    let mut body = &mul_file[0x800..];
-    let mut audio_slices = (0..channels).map(|_| vec![]).collect::<Vec<_>>();
-    let mut data_slices = vec![];
-    let mut audio_chunks = 0;
-    let mut padding_chunks = 0;
-    while let Some(current_chunk) = Chunk::parse(&mut body) {
-        match current_chunk {
-            Chunk::Audio { size } => {
-                audio_chunks += 1;
-                let split_size = size / channels;
-                let bytes = get_bytes(&mut body, size as usize);
-                for (i, slice) in bytes.chunks(split_size as usize).enumerate() {
-                    audio_slices[i].push(slice);
+        let sample_rate = u32::from_le_bytes([mul_file[0], mul_file[1], mul_file[2], mul_file[3]]);
+        let channels = u32::from_le_bytes([mul_file[12], mul_file[13], mul_file[14], mul_file[15]]);
+
+        let mut body = &mul_file[0x800..];
+        let mut audio_slices = (0..channels).map(|_| vec![]).collect::<Vec<_>>();
+        let mut data_slices = vec![];
+        let mut audio_chunks = 0;
+        let mut padding_chunks = 0;
+        while let Some(current_chunk) = Chunk::parse(&mut body) {
+            match current_chunk {
+                Chunk::Audio { size } => {
+                    audio_chunks += 1;
+                    let split_size = size / channels;
+                    let bytes = get_bytes(&mut body, size as usize);
+                    for (i, slice) in bytes.chunks(split_size as usize).enumerate() {
+                        audio_slices[i].push(slice);
+                    }
+                }
+                Chunk::Data { size } => data_slices.push(get_bytes(&mut body, size as usize)),
+                Chunk::Padding { size } => {
+                    padding_chunks += 1;
+                    get_bytes(&mut body, size as usize);
                 }
             }
-            Chunk::Data { size } => data_slices.push(get_bytes(&mut body, size as usize)),
-            Chunk::Padding { size } => {
-                padding_chunks += 1;
-                get_bytes(&mut body, size as usize);
+        }
+
+        let project_name = file_path.file_stem().unwrap().to_string_lossy();
+        let output_dir = file_path.with_extension("");
+        std::fs::create_dir(&output_dir).unwrap();
+        if !audio_slices.is_empty() {
+            for (i, slices) in audio_slices.iter().enumerate() {
+                let mut out = File::create(format!(
+                    "{}/{}_audio_ch{i}.bin",
+                    output_dir.to_string_lossy(),
+                    project_name
+                ))
+                .unwrap();
+                out.write_all(
+                    &slices
+                        .iter()
+                        .copied()
+                        .flatten()
+                        .copied()
+                        .collect::<Vec<_>>(),
+                )
+                .unwrap();
             }
         }
-    }
-
-    let project_name = args.input.file_stem().unwrap().to_string_lossy();
-    let output_dir = args.output.unwrap_or_else(|| args.input.with_extension(""));
-    std::fs::create_dir(&output_dir).unwrap();
-    if !audio_slices.is_empty() {
-        for (i, slices) in audio_slices.iter().enumerate() {
+        if !data_slices.is_empty() {
             let mut out = File::create(format!(
-                "{}/{}_audio_ch{i}.bin",
+                "{}/{}_data.bin",
                 output_dir.to_string_lossy(),
                 project_name
             ))
             .unwrap();
             out.write_all(
-                &slices
+                &data_slices
                     .iter()
-                    .copied()
-                    .flatten()
+                    .flat_map(|data| data.iter())
                     .copied()
                     .collect::<Vec<_>>(),
             )
             .unwrap();
         }
-    }
-    if !data_slices.is_empty() {
-        let mut out = File::create(format!(
-            "{}/{}_data.bin",
-            output_dir.to_string_lossy(),
-            project_name
+
+        let mut rate_file = File::create(format!(
+            "{}/{project_name}_rate.txt",
+            output_dir.to_string_lossy()
         ))
         .unwrap();
-        out.write_all(
-            &data_slices
-                .iter()
-                .flat_map(|data| data.iter())
-                .copied()
-                .collect::<Vec<_>>(),
-        )
-        .unwrap();
-    }
+        for i in 0..channels {
+            write!(
+                &mut rate_file,
+                "{project_name}_audio_ch{i}.bin 1 {sample_rate} 0 16\r\n",
+            )
+            .unwrap();
+        }
 
-    let mut rate_file = File::create(format!(
-        "{}/{project_name}_rate.txt",
-        output_dir.to_string_lossy()
-    ))
-    .unwrap();
-    for i in 0..channels {
-        write!(
-            &mut rate_file,
-            "{project_name}_audio_ch{i}.bin 1 {sample_rate} 0 16\r\n",
-        )
-        .unwrap();
+        println!("MUL file");
+        println!("Audio channels: {channels}");
+        println!("Audio sample rate: {sample_rate}");
+        println!(
+            "Total chunks: {}",
+            data_slices.len() + audio_chunks + padding_chunks
+        );
+        println!("Data chunks: {}", data_slices.len());
+        println!("Audio chunks: {audio_chunks}");
+        println!("Padding chunks: {padding_chunks}");
     }
-
-    println!("MUL file");
-    println!("Audio channels: {channels}");
-    println!("Audio sample rate: {sample_rate}");
-    println!(
-        "Total chunks: {}",
-        data_slices.len() + audio_chunks + padding_chunks
-    );
-    println!("Data chunks: {}", data_slices.len());
-    println!("Audio chunks: {audio_chunks}");
-    println!("Padding chunks: {padding_chunks}");
 }
 
 enum Chunk {
