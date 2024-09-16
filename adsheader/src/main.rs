4b04bd9de595360ce9c91ea178b976888cc2a2b9
SalsaGal <fielddashos@gmail.com>
2024-09-16 18:36:34 +1000

    Add batching to adsheader

--- adsheader/src/main.rs
+++ adsheader/src/main.rs
@@ -17,27 +17,40 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let mut file = std::fs::read(&args.input).unwrap();
-    let file_len = file.len();
+    let file_paths: &mut dyn Iterator<Item = PathBuf> = if args.input.is_dir() {
+        &mut args
+            .input
+            .read_dir()
+            .unwrap()
+            .flatten()
+            .map(|dir| dir.path())
+    } else {
+        &mut std::iter::once(args.input.clone())
+    };
 
-    file.splice(
-        0..0,
-        [
-            [0x53, 0x53, 0x68, 0x64],
-            [0x18, 0, 0, 0],
-            args.format.to_le_bytes(),
-            args.sample_rate.to_le_bytes(),
-            args.channels.to_le_bytes(),
-            args.interleave.to_le_bytes(),
-            [0xff; 4],
-            [0xff; 4],
-            [0x53, 0x53, 0x62, 0x64],
-            (file_len as u32).to_le_bytes(),
-        ]
-        .into_iter()
-        .flatten(),
-    );
+    for file_path in file_paths {
+        let mut file = std::fs::read(&file_path).unwrap();
+        let file_len = file.len();
 
-    let mut output = File::create(args.input.with_extension("ads")).unwrap();
-    output.write_all(&file).unwrap();
+        file.splice(
+            0..0,
+            [
+                [0x53, 0x53, 0x68, 0x64],
+                [0x18, 0, 0, 0],
+                args.format.to_le_bytes(),
+                args.sample_rate.to_le_bytes(),
+                args.channels.to_le_bytes(),
+                args.interleave.to_le_bytes(),
+                [0xff; 4],
+                [0xff; 4],
+                [0x53, 0x53, 0x62, 0x64],
+                (file_len as u32).to_le_bytes(),
+            ]
+            .into_iter()
+            .flatten(),
+        );
+
+        let mut output = File::create(file_path.with_extension("ads")).unwrap();
+        output.write_all(&file).unwrap();
+    }
 }
