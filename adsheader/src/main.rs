dc9126cf4368e672596cf79a490669837db2f587
SalsaGal <fielddashos@gmail.com>
2024-09-29 00:08:24 +1000

    Add logging to adsheader

--- adsheader/src/main.rs
+++ adsheader/src/main.rs
@@ -1,6 +1,9 @@
-use std::{fs::File, io::Write, path::PathBuf};
+use std::{fs::File, io::Write, path::PathBuf, process::exit};
 
-use core::clap::{self, Parser};
+use core::{
+    clap::{self, Parser},
+    log::{debug, error, info},
+};
 
 #[derive(Parser)]
 #[clap(version)]
@@ -19,11 +22,23 @@ fn main() {
 
     let args = Args::parse();
 
-    let file_paths = core::get_files(&args.input).unwrap();
+    let file_paths = core::get_files(&args.input).unwrap_or_else(|e| {
+        error!("Unable to load paths `{:?}`, aborting: {e}", args.input);
+        exit(1);
+    });
 
     for file_path in file_paths {
-        let mut file = std::fs::read(&file_path).unwrap();
+        info!("Doing {file_path:?}");
+
+        let mut file = match std::fs::read(&file_path) {
+            Ok(file) => file,
+            Err(e) => {
+                error!("Unable to load file `{file_path:?}`, skipping: {e}");
+                continue;
+            }
+        };
         let file_len = file.len();
+        debug!("File length: {file_len}");
 
         file.splice(
             0..0,
@@ -43,7 +58,17 @@ fn main() {
             .flatten(),
         );
 
-        let mut output = File::create(file_path.with_extension("ads")).unwrap();
-        output.write_all(&file).unwrap();
+        let output_path = file_path.with_extension("ads");
+        debug!("Writing to {output_path:?}");
+        let mut output = match File::create(&output_path) {
+            Ok(file) => file,
+            Err(e) => {
+                error!("Unable to write output to `{output_path:?}`, skipping: {e}");
+                continue;
+            }
+        };
+        output.write_all(&file).unwrap_or_else(|e| {
+            error!("Unable to write to file `{output_path:?}`: {e}");
+        });
     }
 }
