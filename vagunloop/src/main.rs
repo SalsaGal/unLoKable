656f6833c811d53988e1070dbd6c85ae43a2e695
SalsaGal <fielddashos@gmail.com>
2024-10-06 19:35:03 +1100

    Add batching to vagunloop

--- vagunloop/src/main.rs
+++ vagunloop/src/main.rs
@@ -6,9 +6,6 @@ use core::clap::{self, Parser};
 struct Args {
     /// The `vag` file to read from.
     input: PathBuf,
-    /// The output directory
-    #[clap(short, long)]
-    output: Option<PathBuf>,
 }
 
 fn main() {
@@ -16,35 +13,36 @@ fn main() {
 
     let args = Args::parse();
 
-    let mut vag_bytes = std::fs::read(&args.input).unwrap();
+    for file_path in core::get_files(&args.input) {
+        let mut vag_bytes = std::fs::read(&file_path).unwrap();
 
-    assert_eq!(
-        &vag_bytes[0..4],
-        &[0x56, 0x41, 0x47, 0x70],
-        "Invalid magic number"
-    );
+        assert_eq!(
+            &vag_bytes[0..4],
+            &[0x56, 0x41, 0x47, 0x70],
+            "Invalid magic number"
+        );
 
-    let mut changed_chunks = 0;
-    for chunk in vag_bytes[48..].chunks_mut(16) {
-        if remove_loop(chunk) {
-            changed_chunks += 1;
+        let mut changed_chunks = 0;
+        for chunk in vag_bytes[48..].chunks_mut(16) {
+            if remove_loop(chunk) {
+                changed_chunks += 1;
+            }
         }
-    }
 
-    if changed_chunks == 0 {
-        println!("No markers found");
-    } else {
-        let out_path = args.output.unwrap_or_else(|| {
-            format!(
-                "{}_unlooped.vag",
-                args.input.with_extension("").to_string_lossy()
-            )
-            .into()
-        });
-        let mut out_file = File::create(out_path).unwrap();
-        out_file.write_all(&vag_bytes).unwrap();
+        if changed_chunks == 0 {
+            println!("No markers found");
+        } else {
+            let out_path = {
+                format!(
+                    "{}_unlooped.vag",
+                    file_path.with_extension("").to_string_lossy()
+                )
+            };
+            let mut out_file = File::create(out_path).unwrap();
+            out_file.write_all(&vag_bytes).unwrap();
 
-        println!("{changed_chunks} markers removed");
+            println!("{changed_chunks} markers removed");
+        }
     }
 }
 
