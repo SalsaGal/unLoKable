15af369e4088cf51a10ba50b6ec76f820b5bdaf7
SalsaGal <fielddashos@gmail.com>
2024-10-06 19:15:19 +1100

    Add batching to vagheader

--- vagheader/src/main.rs
+++ vagheader/src/main.rs
@@ -11,8 +11,6 @@ struct Args {
     long: bool,
     #[clap(long)]
     short: bool,
-    #[clap(short, long)]
-    output: Option<PathBuf>,
 }
 
 fn main() {
@@ -20,38 +18,36 @@ fn main() {
 
     let args = Args::parse();
 
-    let file = std::fs::read(&args.input).unwrap();
-    let file_len = file.len();
+    for file_path in core::get_files(&args.input) {
+        let file = std::fs::read(&file_path).unwrap();
+        let file_len = file.len();
 
-    let mut output = File::create(
-        args.output
-            .unwrap_or_else(|| args.input.with_extension("vag")),
-    )
-    .unwrap();
+        let mut output = File::create(file_path.with_extension("vag")).unwrap();
 
-    output
-        .write_all(
-            &[
-                [0x56, 0x41, 0x47, 0x70],
-                [0x0, 0x0, 0x0, 0x3],
-                [0; 4],
-                (file_len as u32).to_be_bytes(),
-                (args.sample_rate.get().to_be_bytes()),
-                [0; 4],
-                [0; 4],
-                [0; 4],
-                [0; 4],
-                [0; 4],
-                [0; 4],
-                [0; 4],
-            ]
-            .into_iter()
-            .flatten()
-            .collect::<Vec<_>>(),
-        )
-        .unwrap();
-    if !args.short {
-        output.write_all(&[0; 16]).unwrap();
+        output
+            .write_all(
+                &[
+                    [0x56, 0x41, 0x47, 0x70],
+                    [0x0, 0x0, 0x0, 0x3],
+                    [0; 4],
+                    (file_len as u32).to_be_bytes(),
+                    (args.sample_rate.get().to_be_bytes()),
+                    [0; 4],
+                    [0; 4],
+                    [0; 4],
+                    [0; 4],
+                    [0; 4],
+                    [0; 4],
+                    [0; 4],
+                ]
+                .into_iter()
+                .flatten()
+                .collect::<Vec<_>>(),
+            )
+            .unwrap();
+        if !args.short {
+            output.write_all(&[0; 16]).unwrap();
+        }
+        output.write_all(&file).unwrap();
     }
-    output.write_all(&file).unwrap();
 }
