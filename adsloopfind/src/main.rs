db95f8033387911e06dfd3d4053a1bb400ab5b67
SalsaGal <fielddashos@gmail.com>
2024-10-13 20:09:35 +1100

    Add tests to adsloopfind

--- adsloopfind/src/main.rs
+++ adsloopfind/src/main.rs
@@ -1,13 +1,8 @@
-use std::{
-    fs::File,
-    io::Write,
-    path::{Path, PathBuf},
-    process::exit,
-};
+use std::{fs::File, io::Write, path::PathBuf, process::exit};
 
 use core::{
     clap::{self, Parser},
-    log::error,
+    log::{error, info},
 };
 
 const MAGIC_NUMBER: [u8; 4] = [0x53, 0x53, 0x68, 0x64];
@@ -36,15 +31,17 @@ fn main() {
     });
 
     for path in files {
-        if let Some((lb, le)) = std::fs::read(&path)
+        info!("Finding in {path:?}");
+
+        if let Some((loop_begin, loop_end)) = std::fs::read(&path)
             .inspect_err(|e| {
                 error!("Unable to read file {path:?}, skipping: {e}");
             })
             .ok()
-            .and_then(|contents| find_loops(&path, &contents))
+            .and_then(|contents| find_loops(&contents))
         {
             let text = format!(
-                "{lb} {le} {}\r\n",
+                "{loop_begin} {loop_end} {}\r\n",
                 path.with_extension("wav")
                     .file_name()
                     .unwrap()
@@ -59,10 +56,10 @@ fn main() {
     }
 }
 
-fn find_loops(path: &Path, ads_file: &[u8]) -> Option<(u32, u32)> {
+fn find_loops(ads_file: &[u8]) -> Option<(u32, u32)> {
     if ads_file[0..4] != MAGIC_NUMBER {
         error!(
-            "Invalid magic number, expected {MAGIC_NUMBER:?}, found {:?}, in file {path:?}",
+            "Invalid magic number, expected {MAGIC_NUMBER:?}, found {:?}",
             &ads_file[0..4]
         );
         return None;
@@ -95,4 +92,23 @@ fn find_loops(path: &Path, ads_file: &[u8]) -> Option<(u32, u32)> {
 
 fn load_bytes(bytes: &[u8]) -> u32 {
     u32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]])
+}
+
+#[test]
+fn load_bytes_test() {
+    assert_eq!(load_bytes(&[0, 0, 0, 0]), 0);
+    assert_eq!(load_bytes(&[0, 0, 0, 1]), 0x01000000);
+    assert_eq!(load_bytes(&[1, 0, 0, 1]), 0x01000001);
+}
+
+#[test]
+fn without_loop() {
+    let loops = find_loops(include_bytes!("../tests/withoutloop.ads"));
+    assert_eq!(loops, None);
+}
+
+#[test]
+fn with_loop() {
+    let loops = find_loops(include_bytes!("../tests/withloop.ads"));
+    assert_eq!(loops, Some((896, 1791)));
 }
